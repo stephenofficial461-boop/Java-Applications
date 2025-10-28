dfbc84fc1fe06d65030356d69638dbc64b3100c8
stephenofficial461-boop <stephenofficial461@gmail.com>
2025-10-28 12:46:11 +0100

    operatorPart1

--- OpeartorPart1.java
+++ OpeartorPart1.java
@@ -1,21 +1,58 @@
-public class operators1{
-	public static void main(String[] args){
-		int num = 50;
-		
-		num += 8;
-		System.out.printf("The value of num is %d%n",num);
-		
-		num -= 7;
-		System.out.printf("The value of num is %d%n",num);
-		
-	    num *= 10;
-		System.out.printf("The value of num is %d%n",num);
-		
-		num /=5;
-		System.out.printf("The value of num is %d%n",num);
-		
-		
-		num %= 3;
-		System.out.printf("The value of num is %d%n",num);
-	}
+public class OperatorPart1 {
+    public static void main(String[] args) {
+        int num = 50;
+        
+        num += 8;
+        System.out.printf("The value of num is %d%n", num);
+        
+        num -= 7;
+        System.out.printf("The value of num is %d%n", num);
+        
+        num *= 10;
+        System.out.printf("The value of num is %d%n", num);
+        
+        num /= 5;
+        System.out.printf("The value of num is %d%n", num);
+        
+        num %= 3;
+        System.out.printf("The value of num is %d%n", num);
+        
+        
+        // Arithmetic operators
+        int num1 = 10;
+        int num2 = 4;
+        String name = "Martins";
+        
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double divide = (double) num1 / num2;
+        int modulus = num1 % num2;
+        
+        System.out.printf("Sum is %d%n", sum);
+        System.out.printf("Product is %d%n", product);
+        System.out.printf("Difference between num1 and num2 is %d%n", difference);
+        System.out.printf("%s's total sum is %d%n", name, sum);
+        System.out.printf("Division result is %.2f%n", divide);
+        System.out.printf("Modulus is %d%n", modulus);
+        
+        
+        // Relational operators
+        int firstNum = 12;
+        int secondNum = 15;
+        
+        boolean isGreater = firstNum > secondNum;
+        boolean isLesser = firstNum < secondNum;
+        boolean isLessThanOrEqualTo = firstNum <= secondNum;
+        boolean isGreaterThanOrEqualTo = firstNum >= secondNum;
+        boolean isNotEqualTo = firstNum != secondNum;
+        boolean isEqualTo = firstNum == secondNum;
+        
+        System.out.printf("Is firstNum > secondNum? %b%n", isGreater);
+        System.out.printf("Is firstNum < secondNum? %b%n", isLesser);
+        System.out.printf("Is firstNum >= secondNum? %b%n", isGreaterThanOrEqualTo);
+        System.out.printf("Is firstNum <= secondNum? %b%n", isLessThanOrEqualTo);
+        System.out.printf("Is firstNum != secondNum? %b%n", isNotEqualTo);
+        System.out.printf("Is firstNum == secondNum? %b%n", isEqualTo);
+    }
 }
